21662e672cf33d1392e45cc0bf9865a37abac489
Antwan Maddox <antwan.maddox@agilethought.com>
2019-10-04 16:33:34 -0400

    Added Selenium libraries and code to unit test class

--- UnitTestProject1/UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -1,14 +1,30 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace UnitTestProject1
 {
     [TestClass]
     public class UnitTest1
     {
+        public IWebDriver _driver = null;
         [TestMethod]
         public void TestMethod1()
         {
+            _driver.Navigate().GoToUrl("http://www.espn.com");
+        }
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _driver = new ChromeDriver();
+
+        }
+        [TestCleanup]
+        public void Teardown()
+        {
+
         }
     }
 }
